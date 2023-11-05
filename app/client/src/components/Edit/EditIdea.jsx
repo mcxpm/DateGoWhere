6cccc4bb32896c49af950f736ef556598e3f6aed
ackselz <tanshaochong48@gmail.com>
2023-11-05 21:51:52 +0800

    fix: overflow in edit and create

--- app/client/src/components/Edit/EditIdea.jsx
+++ app/client/src/components/Edit/EditIdea.jsx
@@ -79,118 +79,127 @@ const EditIdea = () => {
 
     return (
         <>
-            <Paper p={'md'} m={'xs'} withBorder shadow="xl" className={classes.leftPanel}>
-                <Stack gap={'sm'}>
-                    <TextInput
-                        label="Give your date a name!"
-                        placeholder="Date Name"
-                        withAsterisk
-                        value={info.title}
-                        size="xs"
-                        onChange={(e) =>
-                            setInfo((prevInfo) => ({
-                                ...prevInfo,
-                                title: e.target.value,
-                            }))
-                        }
-                    />
-                    <TextInput
-                        label="Description"
-                        placeholder="Description"
-                        value={info.description}
-                        size="xs"
-                        onChange={(e) =>
-                            setInfo((prevInfo) => ({
-                                ...prevInfo,
-                                description: e.target.value,
-                            }))
-                        }
-                    />
-                    <TextInput
-                        label="Thumbail Image URL"
-                        placeholder="URL"
-                        value={info.image}
-                        size="xs"
-                        onChange={(e) =>
-                            setInfo((prevInfo) => ({
-                                ...prevInfo,
-                                image: e.target.value,
-                            }))
-                        }
-                    />
-                    <Flex align="flex-end" gap="xl">
-                        <MultiSelect
-                            variant="filled"
+            <Paper
+                p={'md'}
+                m={'xs'}
+                withBorder
+                shadow="xl"
+                className={classes.leftPanel}
+                style={{ overflow: 'hidden' }}
+            >
+                <Stack gap={'sm'} style={{ overflow: 'hidden' }}>
+                    <Stack gap={'sm'} style={{ overflowY: 'auto' }}>
+                        <TextInput
+                            label="Give your date a name!"
+                            placeholder="Date Name"
+                            withAsterisk
+                            value={info.title}
                             size="xs"
-                            label="Date Tags"
-                            placeholder="Pick Category"
-                            data={[
-                                'Romantic',
-                                'Outdoor',
-                                'Food',
-                                'Sport',
-                                'Dance',
-                                'Cultural',
-                            ]}
-                            searchable
-                            nothingFoundMessage="Nothing found..."
-                            value={info.tags}
-                            onChange={(values) =>
-                                setInfo((prevInfo) => ({ ...prevInfo, tags: values }))
-                            }
-                        />
-
-                        <Switch
-                            defaultChecked
-                            py={6}
-                            color="pink"
-                            label="Make this date idea public"
-                            size="xs"
-                            checked={info.isPublic}
                             onChange={(e) =>
                                 setInfo((prevInfo) => ({
                                     ...prevInfo,
-                                    isPublic: e.target.checked,
+                                    title: e.target.value,
                                 }))
                             }
                         />
-                    </Flex>
-
-                    {activityList.map((activity, idx) => {
-                        return !isEditingList[idx] ? (
-                            <Paper key={idx} p="xs" shadow="none" withBorder>
-                                <Box className={classes.card}>
-                                    <Box>
-                                        <Text size="xs" c={'dimmed'} fw={500}>
-                                            {activity.start} - {activity.end}{' '}
-                                        </Text>
-                                        <Text truncate="end">
-                                            <Text component="span" fw={700}>
-                                                {activity.name}
-                                            </Text>
-                                            &nbsp;@&nbsp;
-                                            {activity.location.name
-                                                ? activity.location.name
-                                                : activity.location.description}
-                                        </Text>
-                                    </Box>
-                                    <MdEdit
-                                        style={{ placeSelf: 'center' }}
-                                        onClick={() => handleEditActivity(idx)}
-                                        color="grey"
-                                    />
-                                </Box>
-                            </Paper>
-                        ) : (
-                            <ActivityForm
-                                key={idx}
-                                activity={activity}
-                                idx={idx}
-                                handleDiscardActivity={handleDiscardActivity}
-                                handleSaveActivity={handleSaveActivity}
+                        <TextInput
+                            label="Description"
+                            placeholder="Description"
+                            value={info.description}
+                            size="xs"
+                            onChange={(e) =>
+                                setInfo((prevInfo) => ({
+                                    ...prevInfo,
+                                    description: e.target.value,
+                                }))
+                            }
+                        />
+                        <TextInput
+                            label="Thumbail Image URL"
+                            placeholder="URL"
+                            value={info.image}
+                            size="xs"
+                            onChange={(e) =>
+                                setInfo((prevInfo) => ({
+                                    ...prevInfo,
+                                    image: e.target.value,
+                                }))
+                            }
+                        />
+                        <Flex align="flex-end" gap="xl">
+                            <MultiSelect
+                                variant="filled"
+                                size="xs"
+                                label="Date Tags"
+                                placeholder="Pick Category"
+                                data={[
+                                    'Romantic',
+                                    'Outdoor',
+                                    'Food',
+                                    'Sport',
+                                    'Dance',
+                                    'Cultural',
+                                ]}
+                                searchable
+                                nothingFoundMessage="Nothing found..."
+                                value={info.tags}
+                                onChange={(values) =>
+                                    setInfo((prevInfo) => ({ ...prevInfo, tags: values }))
+                                }
                             />
-                        );
-                    })}
+
+                            <Switch
+                                defaultChecked
+                                py={6}
+                                color="pink"
+                                label="Make this date idea public"
+                                size="xs"
+                                checked={info.isPublic}
+                                onChange={(e) =>
+                                    setInfo((prevInfo) => ({
+                                        ...prevInfo,
+                                        isPublic: e.target.checked,
+                                    }))
+                                }
+                            />
+                        </Flex>
+
+                        {activityList.map((activity, idx) => {
+                            return !isEditingList[idx] ? (
+                                <Paper key={idx} p="xs" shadow="none" withBorder>
+                                    <Box className={classes.card}>
+                                        <Box>
+                                            <Text size="xs" c={'dimmed'} fw={500}>
+                                                {activity.start} - {activity.end}{' '}
+                                            </Text>
+                                            <Text truncate="end">
+                                                <Text component="span" fw={700}>
+                                                    {activity.name}
+                                                </Text>
+                                                &nbsp;@&nbsp;
+                                                {activity.location.name
+                                                    ? activity.location.name
+                                                    : activity.location.description}
+                                            </Text>
+                                        </Box>
+                                        <MdEdit
+                                            style={{ placeSelf: 'center' }}
+                                            onClick={() => handleEditActivity(idx)}
+                                            color="grey"
+                                        />
+                                    </Box>
+                                </Paper>
+                            ) : (
+                                <ActivityForm
+                                    key={idx}
+                                    activity={activity}
+                                    idx={idx}
+                                    handleDiscardActivity={handleDiscardActivity}
+                                    handleSaveActivity={handleSaveActivity}
+                                />
+                            );
+                        })}
+                    </Stack>
                     <Button
                         onClick={handleAddActivity}
                         fullWidth
